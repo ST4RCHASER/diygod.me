90034d564061a5e2dd41d52c9b21bb7f64b04b36
_StarChaser <underscore@starchaser.me>
2021-01-05 19:35:45 +0700

    resize header image

--- themes/sagiri/src/aplayer.js
+++ themes/sagiri/src/aplayer.js
@@ -3,16 +3,11 @@ if (!NexT.utils.isMobile()) {
     element: document.getElementById('aplayer-fixed'),
     autoplay: false,
     fixed: true,
+    mutex: true,
     theme: '#b19cd9',
-    audio: [{
-      name: 'Can now, Can now',
-      artist: 'Study',
-      url: 'https://storage.starchaser.workers.dev/0:/songs/cannow.mp3',
-      cover: 'https://storage.starchaser.workers.dev/0:/songs/cannow.jpg',
-  }],
 });
   $.ajax({
-    url: 'https://api.i-meto.com/meting/api?server=netease&type=playlist&id=35798529',
+    url: 'https://storage.starchaser.workers.dev/0:/songs/lists.json',
     success: function (list) {
       apFixed.list.add(list);
     }
